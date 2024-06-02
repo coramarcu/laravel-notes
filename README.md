22e3c871e4995099c1c3052c0af67361228b40f3
Cora Marcu <coramarcu@gmail.com>
2024-06-02 15:46:28 +0100

    finish views for create, edit and show notes

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 To start the back end server run:
 `php artisan serve`
 
-To start the front end server run:
+To start the front end server in development mode run:
 `npm run dev`
+
+To build the front end asset files for produciton run:
+`npm run build`
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,11 +6,8 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <!-- Styles -->
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         {{ $slot }}
